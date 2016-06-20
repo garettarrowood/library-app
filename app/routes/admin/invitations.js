62e0971f4fcb9890ece4dc9aaf538e89ba93ad4f
Garett Arrowood <garettarrowood@gmail.com>
2016-06-19 22:23:12 -0400

    Add delete buttons to admin data displays

--- app/routes/admin/invitations.js
+++ app/routes/admin/invitations.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('invitation');
+  },
+
+  actions: {
+
+    deleteInvitation(invitation) {
+      let confirmation = confirm('Are you sure you want to delete this invitation?');
+
+      if (confirmation) {
+        invitation.destroyRecord();
+      }
+    }
   }
 });
