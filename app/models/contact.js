7cfec4869d881c5c0e894468638122f0a728ff73
Garett Arrowood <garettarrowood@gmail.com>
2016-06-21 20:58:32 -0400

    Refactor out Contact controller

--- app/models/contact.js
+++ app/models/contact.js
@@ -1,7 +1,15 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+import Ember from 'ember';
 
 export default Model.extend({
   email: attr('string'),
-  message: attr('string')
+  message: attr('string'),
+
+  isValidEmail: Ember.computed.match('email', /^.+@.+\..+$/),
+  isMessageEnoughLong: Ember.computed.gte('message.length', 5),
+
+  isValid: Ember.computed.and('isValidEmail', 'isMessageEnoughLong'),
+  isDisabled: Ember.computed.not('isValid')
+
 });
--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model() {
+    return this.store.createRecord('contact');
+  },
+
+  actions: {
+
+    sendMessage(newContact) {
+      newContact.save().then(() => this.controller.set('responseMessage', true));
+    },
+
+    willTransition() {
+      this.controller.get('model').rollbackAttributes();
+    }
+  }
 });
