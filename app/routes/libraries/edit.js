9dbdbb31eb514c46ebf6a1331313072f657f6139
Garett Arrowood <garettarrowood@gmail.com>
2016-06-25 14:22:19 -0400

    Use renderTemplate, setupController, and a form.hbs to refactor out new and edit templates

--- app/routes/libraries/edit.js
+++ app/routes/libraries/edit.js
@@ -6,14 +6,23 @@ export default Ember.Route.extend({
     return this.store.findRecord('library', params.library_id);
   },
 
-  actions: {
+  setupController(controller, model) {
+    this._super(controller, model);
 
+    controller.set('title', 'Edit library');
+    controller.set('buttonLabel', 'Save changes');
+  },
+
+  renderTemplate() {
+    this.render('libraries/form');
+  },
+
+  actions: {
     saveLibrary(newLibrary) {
       newLibrary.save().then(() => this.transitionTo('libraries'));
     },
 
     willTransition(transition) {
-
       let model = this.controller.get('model');
 
       if (model.get('hasDirtyAttributes')) {
--- app/routes/libraries/new.js
+++ app/routes/libraries/new.js
@@ -5,14 +5,23 @@ export default Ember.Route.extend({
     return this.store.createRecord('library');
   },
 
+  setupController: function (controller, model) {
+    this._super(controller, model);
+
+    controller.set('title', 'Create a new library');
+    controller.set('buttonLabel', 'Create');
+  },
+
+  renderTemplate() {
+    this.render('libraries/form');
+  },
+
   actions: {
     saveLibrary(newLibrary) {
       newLibrary.save().then(() => this.transitionTo('libraries'));
     },
 
     willTransition() {
-      // rollbackAttributes() removes the record from the store
-      // if the model 'isNew'
       this.controller.get('model').rollbackAttributes();
     }
   }
