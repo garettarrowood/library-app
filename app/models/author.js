f1b9a758fa1a267949e82413e5db632649309697
Garett Arrowood <garettarrowood@gmail.com>
2016-06-25 18:00:04 -0400

    Add Author, Books, with belongs/hasMany relationship, and admin/seeder page to display number of records

--- app/models/author.js
+++ app/models/author.js
@@ -1,14 +1,8 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { hasMany } from 'ember-data/relationships';
-import Ember from 'ember';
 
 export default Model.extend({
   name: attr('string'),
-  address: attr('string'),
-  phone: attr('string'),
-
-  books: hasMany('book'),
-
-  isValid: Ember.computed.notEmpty('name')
+  books: hasMany('book')
 });
