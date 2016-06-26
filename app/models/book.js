c30105ea84bf057550a5da94dd2ad826afd863dd
Garett Arrowood <garettarrowood@gmail.com>
2016-06-26 14:05:49 -0400

    Use faker.js to create/seed random Libraries, Authors and Books

--- app/models/book.js
+++ app/models/book.js
@@ -1,10 +1,33 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo } from 'ember-data/relationships';
+import Faker from 'faker';
 
 export default Model.extend({
   title: attr('string'),
   releaseYear: attr('date'),
-  library: belongsTo('library'),
-  author: belongsTo('author')
+
+  library: belongsTo('library', {inverse: 'books', async: true}),
+  author: belongsTo('author', {inverse: 'books', async: true}),
+
+  randomize(author, library) {
+    this.set('title', this._bookTitle());
+    this.set('author', author);
+    this.set('releaseYear', this._randomYear());
+    this.set('library', library);
+
+    return this;
+  },
+
+  _bookTitle() {
+    return `${Faker.commerce.productName()} Ed. ${this._getRandomArbitrary(1, 100)}`;
+  },
+
+  _randomYear() {
+    return new Date(this._getRandomArbitrary(1930, 2016));
+  },
+
+  _getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
 });
--- app/models/library.js
+++ app/models/library.js
@@ -2,13 +2,22 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { hasMany } from 'ember-data/relationships';
 import Ember from 'ember';
+import Faker from 'faker';
 
 export default Model.extend({
   name: attr('string'),
   address: attr('string'),
   phone: attr('string'),
 
-  books: hasMany('book'),
+  books: hasMany('book', {inverse: 'library', async: true}),
 
-  isValid: Ember.computed.notEmpty('name')
+  isValid: Ember.computed.notEmpty('name'),
+
+  randomize() {
+    this.set('name', Faker.name.lastName() + ' ' + Faker.name.jobDescriptor() + ' Library');
+    this.set('address', Faker.address.streetAddress(true));
+    this.set('phone', Faker.phone.phoneNumber());
+
+    return this;
+  }
 });
